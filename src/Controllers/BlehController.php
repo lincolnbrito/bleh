7e76e939180aca870bec413ab4c5d51dcea0c64c
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-06-20 17:07:57 -0300

    add controller and routes

---
+++ src/Controllers/BlehController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace LincolnBrito\Bleh\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class BlehController extends Controller{
+    public function foo(){
+        return 'The controller works!';
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -1,1 +1,4 @@
 <?php
+Route::group(['namespace'=>'LincolnBrito\Bleh\Controllers', 'prefix'=>'blehdemo'], function(){
+    Route::get('foo', 'BlehController@foo');
+});
